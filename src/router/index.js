d99a029dccb4dd2df83b80db828e73893f1cdf16
Bxrbin <18538092320@163.com>
2021-10-12 23:07:09 +0800

    twelve mark

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,32 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
-  {
+const routes = [{
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/Login',
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/Login',
+    name: 'Login',
+    component: () => import( /* webpackChunkName: "login" */ '../views/Login.vue')
+  },
+  {
+    path: '/Home',
+    name: 'Home',
+    component: () => import( /* webpackChunkName: "Home" */ '../views/Home.vue')
   }
 ]
-
 const router = new VueRouter({
-  mode: 'history',
+  // mode: 'history',
   base: process.env.BASE_URL,
   routes
 })
-
+router.beforeEach((to,from,next)=>{
+  if(to.path==='/Login')return next();
+  const tokenStr=window.sessionStorage.getItem('token')
+  if(!tokenStr)return next('/Login')
+  next()
+})
 export default router
